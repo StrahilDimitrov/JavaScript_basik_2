e5d39eaac3ebc5e4007bb7b11d02507f08f2d6df
Strahil527 <hristovstrahil5@gmail.com>
2026-08-18 14:25:07 +0300

    Create Animal class.js

---
+++ Animal class.js
@@ -0,0 +1,22 @@
+function animal(input) {
+    let typeAnimal = input[0];
+
+    switch (typeAnimal) {
+        case "dog":
+            console.log("mammal");
+            break;
+
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+            console.log("reptile");
+            break;
+
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+
+animal(["dog"])
+animal(["snake"])
